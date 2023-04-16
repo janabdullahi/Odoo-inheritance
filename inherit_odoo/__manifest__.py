7e75c5066ef27ae3eec03f577c9ebd0781c7dfcd
janabdullahi <abdullahi@netlinks.af>
2023-04-16 12:03:32 +0430

    removed unnecessary codes

--- inherit_odoo/__manifest__.py
+++ inherit_odoo/__manifest__.py
@@ -3,33 +3,22 @@
     'name': "inherit_odoo",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Odoo Inheritance""",
 
     'description': """
-        Long description of module's purpose
+        Odoo Inheritance
     """,
 
     'author': "My Company",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    
 }
