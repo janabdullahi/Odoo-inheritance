426cbbf0605128a5b477200040b9bb3f56d3d8a5
janabdullahi <abdullahi@netlinks.af>
2023-04-18 13:09:03 +0430

    adding new field using inheritance in sale order

--- inherit_odoo/models/models.py
+++ inherit_odoo/models/models.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class SaleOrderInheriting(models.Model):
+    _inherit = 'sale.order'
 
-# class inherit_odoo(models.Model):
-#     _name = 'inherit_odoo.inherit_odoo'
-#     _description = 'inherit_odoo.inherit_odoo'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    #[inheritance] : Adding New Field
+    name = fields.Char('Inherited field name')
